4140b7ac396a7e1535a4c469b0db4b5d99e1c2ee
garevna <garevna@mail.ru>
2018-06-13 09:06:13 +0300

    add block-scheme

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var circle = document.querySelector ( ".circle" )
+var scheme = document.querySelector ( "#block-scheme" )
 circle.style.visibility = "visible"
 
 var button = document.querySelector ( "button" )
 
 button.onclick = function ( event ) {
-        circle.style.visibility = circle.style.visibility === "visible" ?
+        scheme.style.visibility = scheme.style.visibility === "visible" ?
                                      "hidden" : "visible"
         button.innerHTML = button.innerHTML === "Показать" ? "Скрыть" : "Показать"
 }
